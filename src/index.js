80d5d8e875ba01c9390e42c3df292bcaac16dd0b
Jacob Zamano <jacobzamano@pop-os.localdomain>
2020-06-29 21:15:10 -0700

    wow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
 import ReactDOM from 'react-dom';
 import Home from './Pages/Home/Home';
 import * as serviceWorker from './serviceWorker';
+
+//Variable inputs
+var Router = require("react-router").Router;
+var Route = require("react-router").Route;
+var Switch = require("react-router").Switch;
+
+const root = ({ref})
 
 ReactDOM.render(
   <React.StrictMode>
